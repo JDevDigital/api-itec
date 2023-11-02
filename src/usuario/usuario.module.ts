9cac94055c063b8a425e4c2b7738086e622a6a26
kaio <kaioresendeoficial@outlook.com>
2023-11-02 11:15:14 -0300

    :construction: ApiError > Module

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioRepository } from './repository/usuario.repository';
+import { ApiError } from 'src/config/ApiError';
 
 @Module({
-  providers: [UsuarioService, UsuarioRepository],
+  providers: [UsuarioService, UsuarioRepository, ApiError],
   controllers: [UsuarioController],
 })
 export class UsuarioModule {}
